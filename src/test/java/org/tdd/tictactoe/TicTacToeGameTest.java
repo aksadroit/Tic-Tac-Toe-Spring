00e9988d988da1ce61d95b0c81079cbb0c8bc15d
aksadroit <AKSHAT4@GMAIL.COM>
2021-04-20 12:05:37 +0530

    test: extracted string literals as constant for reusability

--- src/test/java/org/tdd/tictactoe/TicTacToeGameTest.java
+++ src/test/java/org/tdd/tictactoe/TicTacToeGameTest.java
@@ -6,11 +6,14 @@ import org.junit.jupiter.api.Test;
 
 class TicTacToeGameTest {
 
+	private static final int POS_1 = 1;
+	private static final char PLAYER_X = 'X';
+	
 	@Test
 	void playerXShouldBeAbleToMakeMoveInAnyPositionOnTheBoardAndIdentifyTheSame() {
 		TicTacToeGame game = new TicTacToeGame();
-		game.placeMoveOnTheBoard(1, 1, 'X');
-		assertEquals('X', game.identifyPlayerAt(1, 1));
+		game.placeMoveOnTheBoard(POS_1, POS_1, PLAYER_X);
+		assertEquals(PLAYER_X, game.identifyPlayerAt(POS_1, POS_1));
 	}
 
 }

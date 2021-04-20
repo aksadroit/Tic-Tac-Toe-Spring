aeae1e723babb0b2f5ad55a6ac3adba2f211aada
aksadroit <AKSHAT4@GMAIL.COM>
2021-04-20 12:35:25 +0530

    test: extracted string literals as constant and removed duplicate blocks

--- src/test/java/org/tdd/tictactoe/TicTacToeGameTest.java
+++ src/test/java/org/tdd/tictactoe/TicTacToeGameTest.java
@@ -2,27 +2,36 @@ package org.tdd.tictactoe;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class TicTacToeGameTest {
 
+	private static final int POS_0 = 0;
 	private static final int POS_1 = 1;
+	
 	private static final char PLAYER_X = 'X';
+	private static final char PLAYER_O = 'O';
+	
+	private TicTacToeGame game;
+	
+	@BeforeEach
+	void init() {
+		game = new TicTacToeGame();
+	}
 	
 	@Test
 	void playerXShouldBeAbleToMakeMoveInAnyPositionOnTheBoardAndIdentifyTheSame() {
-		TicTacToeGame game = new TicTacToeGame();
 		game.placeMoveOnTheBoard(POS_1, POS_1);
 		assertEquals(PLAYER_X, game.identifyPlayerAt(POS_1, POS_1));
 	}
 	
 	@Test
 	void alternativelySwitchBetweenPlayers() {
-		TicTacToeGame game = new TicTacToeGame();
 		game.placeMoveOnTheBoard(POS_1, POS_1);
 		assertEquals(PLAYER_X, game.identifyPlayerAt(POS_1, POS_1));
-		game.placeMoveOnTheBoard(0, POS_1);
-		assertEquals('O', game.identifyPlayerAt(0, POS_1));
+		game.placeMoveOnTheBoard(POS_0, POS_1);
+		assertEquals(PLAYER_O, game.identifyPlayerAt(POS_0, POS_1));
 	}
 	
 	

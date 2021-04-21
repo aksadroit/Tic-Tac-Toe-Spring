99c700aaed590315bc63547b21c040e87c061356
aksadroit <AKSHAT4@GMAIL.COM>
2021-04-21 11:14:37 +0530

    test: extracted string as constant for reusability

--- src/test/java/org/tdd/tictactoe/TicTacToeGameExecutorTest.java
+++ src/test/java/org/tdd/tictactoe/TicTacToeGameExecutorTest.java
@@ -13,19 +13,25 @@ import org.tdd.tictactoe.exception.InvalidUserInputException;
 @SpringBootTest
 public class TicTacToeGameExecutorTest {
 
+	private static final String INPUT_ONE = "1";
+	private static final String INPUT_SPACE = " ";
+	
+	private static final String INPUT_ZERO_ZERO = "0,0";
+	private static final String INVALID_INPUT = "1,";
+	
 	@Autowired
 	TicTacToeGameExecutor gameExecutor;
 	
 	@Test
 	public void shouldReturnTrueIfUserInputAreInvalid() {
-		String[] userInputs = {"1", " "};
+		String[] userInputs = {INPUT_ONE, INPUT_SPACE};
 		
 		assertTrue(gameExecutor.isUserInputInvalid(userInputs));
 	}
 	
 	@Test
 	public void shouldThrowExceptionIfUserInputAreInvalid() throws InvalidUserInputException  {
-		StringBuilder userInputBuilder = new StringBuilder("0,0").append("\n").append("1,");
+		StringBuilder userInputBuilder = new StringBuilder(INPUT_ZERO_ZERO).append("\n").append(INVALID_INPUT);
 		System.setIn(new ByteArrayInputStream(userInputBuilder.toString().getBytes()));
 		
 		assertThrows(InvalidUserInputException.class, () -> gameExecutor.runGame());

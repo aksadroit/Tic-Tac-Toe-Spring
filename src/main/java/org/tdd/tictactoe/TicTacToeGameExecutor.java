6572becb2cc02dddc658e236912ed571523ec554
aksadroit <AKSHAT4@GMAIL.COM>
2021-04-21 11:01:21 +0530

    refactor: extracted string literals as constant for reusability

--- src/main/java/org/tdd/tictactoe/TicTacToeGameExecutor.java
+++ src/main/java/org/tdd/tictactoe/TicTacToeGameExecutor.java
@@ -10,6 +10,14 @@ import org.tdd.tictactoe.exception.InvalidUserInputException;
 @Component
 @Scope(value = ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class TicTacToeGameExecutor {
+	
+	private static final int USER_INPUT_LENGTH = 2;
+	private static final int USER_INPUT_ZERO = 0;
+	private static final int USER_INPUT_ONE = 1;
+	
+	private static final String USER_INPUT_REGEX = "[ ]*\\\\d+[ ]*";
+	private static final String INVALID_USER_INPUT_MESSAGE = "Invalid inputs Passed..!! Please pass the input in the format 1,1";
+	
 
 	public void runGame() throws InvalidUserInputException {
 		Scanner scan = new Scanner(System.in);
@@ -17,7 +25,7 @@ public class TicTacToeGameExecutor {
 			String[] input = scan.nextLine().split(",");
 			if (isUserInputInvalid(input)) {
 				scan.close();
-				throw new InvalidUserInputException("Invalid inputs Passed..!! Please pass the input in the format 1,1");
+				throw new InvalidUserInputException(INVALID_USER_INPUT_MESSAGE);
 			}
 
 		} while (scan.hasNext());
@@ -27,9 +35,9 @@ public class TicTacToeGameExecutor {
 	public boolean isUserInputInvalid(String[] userInputs) {
 		boolean isInvalid = true;
 		
-		if (userInputs.length == 2 
-				&& userInputs[0].matches("[ ]*\\d+[ ]*") 
-				&& userInputs[1].matches("[ ]*\\d+[ ]*")) {
+		if (userInputs.length == USER_INPUT_LENGTH 
+				&& userInputs[USER_INPUT_ZERO].matches(USER_INPUT_REGEX) 
+				&& userInputs[USER_INPUT_ONE].matches(USER_INPUT_REGEX)) {
 			isInvalid = false;
 		}
 		
